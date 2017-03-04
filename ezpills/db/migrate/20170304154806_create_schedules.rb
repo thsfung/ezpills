28c177bf0959bb4ad7f709bfc1cb50b27fbe9003
Tim Fung <thsfung@yahoo.com>
2017-03-04 11:59:42 -0500

    created migration and added cron gem

--- ezpills/db/migrate/20170304154806_create_schedules.rb
+++ ezpills/db/migrate/20170304154806_create_schedules.rb
@@ -2,6 +2,10 @@ class CreateSchedules < ActiveRecord::Migration[5.0]
   def change
     create_table :schedules do |t|
 
+      t.string :name
+      t.integer :duration
+      t.integer :quantity
+      t.time :last_taken, array: true, default: []
       t.timestamps
     end
   end
